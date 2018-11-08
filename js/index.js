3d248fde45b2e34440380bac18f081898a83fdf9
vihai93 <vihaihahaha@gmail.com>
2018-11-08 23:41:05 +0700

    Hai updated

--- js/index.js
+++ js/index.js
@@ -2,5 +2,10 @@ var app = new Vue({
     el: '#app',
     data: {
         message: 'Hello Vue!'
+    },
+    methods: {
+        changeValue:function(event){
+            this.message = event.target.value;
+        }
     }
 });
